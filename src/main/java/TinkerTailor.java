acb319881f66a9c8ad40c6b17d180a9f4ede131b
Seweryn91 <s.talaj@gmail.com>
2019-01-31 15:48:18 +0100

    Implement class Player

--- src/main/java/TinkerTailor.java
+++ src/main/java/TinkerTailor.java
@@ -1,2 +1,19 @@
 public class TinkerTailor {
+
+    private int N; //number of players in circle
+    private int K; //length of sequence
+
+    class Player {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
